1f7099f46d5e3c4e97717c2781cc3e11b6c36753
khomsahwijayakusuma <khomsahwijayakusuma@gmail.com>
2026-03-30 09:40:03 +0700

    fix error undefined variable books pada halaman home

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,25 @@
 <?php
 
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\AuthController;
+use App\Models\Book; // tambahkan ini
 
+Route::get('/', [AuthController::class, 'showLogin'])
+    ->name('login')
+    ->middleware('guest');
 
-Route::get('/', [AuthController::class, 'showLogin'])->name('login')->middleware('guest');
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->name('logout');
 
 Route::middleware('auth')->group(function () {
-   
-Route::get('/home', function () {
-        return view('home');
+
+    Route::get('/home', function () {
+        $books = Book::latest()->take(4)->get(); // ambil 4 buku terbaru
+        return view('home', compact('books')); // kirim ke home.blade.php
     })->name('home');
 
+    Route::resource('books', BookController::class);
 
-Route::resource('books', BookController::class);
-   
 });
