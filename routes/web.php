4a603309012608461273a129602ee12f848f0cb5
khomsahwijayakusuma <khomsahwijayakusuma@gmail.com>
2026-03-30 09:21:52 +0700

    menambahkan fitur login

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,23 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\HomeController; 
+use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-*/
 
-Route::get('/', [HomeController::class, 'index'])->name('home'); 
+Route::get('/', [AuthController::class, 'showLogin'])->name('login')->middleware('guest');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+
+Route::middleware('auth')->group(function () {
+   
+Route::get('/home', function () {
+        return view('home');
+    })->name('home');
+
 
 Route::resource('books', BookController::class);
-Route::resource('categories', CategoryController::class);
+   
+});
